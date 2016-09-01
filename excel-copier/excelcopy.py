5421c62b9d9a1477922ba906f2c4e07edbac1fcb
Chief Keef <pcross2@lion.lmu.edu>
2016-09-01 11:25:58 -0700

    Got rid of prints and did a little variable renaming

--- excel-copier/excelcopy.py
+++ excel-copier/excelcopy.py
@@ -4,18 +4,14 @@ sheetNames = wb.get_sheet_names()
 alphabet = list('abcdefghijklmnopqrstuvwxyz'.upper())
 writeWB = openpyxl.Workbook()
 writeSheet = writeWB.active
-counter = 1
+numericCounter = 1
 alphabetCounter = 0
 for name in sheetNames:
     currentSheet = wb.get_sheet_by_name(name)
     for row in currentSheet:
         for cellObj in row:
-            #print(cellObj.coordinate, cellObj.value)
-            #print(name)
             writeSheet[alphabet[alphabetCounter] + str(counter)] = cellObj.value
-            #print( writeSheet[alphabet[alphabetCounter] + str(counter)].value)
             alphabetCounter += 1
-        #print('End of Row')
-        counter +=1
+        numericCounter +=1
         alphabetCounter = 0
 writeWB.save('export.xlsx')
