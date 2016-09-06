d732420df52ad4ad411866f60cb5d05125760668
Chief Keef <pcross2@lion.lmu.edu>
2016-09-06 10:17:58 -0700

    Added main method and modularized opening files

--- state-reporter/excel.py
+++ state-reporter/excel.py
@@ -7,7 +7,6 @@ wbSave = openpyxl.Workbook()
 wbSave.get_sheet_names()
 outputSheet = wbSave.active
 sheet = masterList.get_sheet_by_name('Sheet1')
-file = open("JUL2017.txt","r")
 output = open("newpeople.txt","w")
 pagebegin = re.compile("LMAP7LST")
 SSN = re.compile("(\d{3}-\d{2}-\d{4})")
@@ -24,7 +23,7 @@ space = re.compile("^\s*$")
 ADR = re.compile("(\d*)")
 streetNameRegex = re.compile("([a-zA-Z]\s?\.?\s?)+\d*")
 ssnList = []
-item = 0
+
 
 #Method checks to see if we are at the top of the page and skips it.
 def pageCheck(file,line):
@@ -45,111 +44,114 @@ for ssn in sheet.columns[3]:
     ssnList.append(ssn.value)
 
 
-
-for line in file:
-    #Skip white space
-    while space.match(line):
-        line = file.next()
-    line = pageCheck(file,line)
-    name = line
-    print(name)
-    line = pageCheck(file,line)
-    ssnLine = file.next()
-    ssnLine = pageCheck(file,ssnLine)
-    foundSSN = SSN.search(ssnLine)
-    foundAddress = address.search(ssnLine)
-    foundApt = apt.search(ssnLine)
-    if foundSSN:
-        personSSN = foundSSN.group(0)
-        print(personSSN)
-    else: 
-        personSSN = False
-        print("Invalid SSN")
-    if foundAddress:
-        personFoundAddress = foundAddress.group(0)
-        print("The address is " + foundAddress.group(0))
-    else:
-        personFoundAddress = "ADDRESS REGEX DID NOT MATCH"
-    if foundApt:
-        personApt = foundApt.group(0)
-        print ("The apartment number is " + personApt)
-    else:
-        personApt = ""
-    cityLine=file.next()
-    line = cityLine
-    line = pageCheck(file,line)
-    foundCityGroup = city.findall(line)
-    if foundCityGroup:
-        personCity = ('').join(foundCityGroup[0])
-        if foundCityGroup[1]:
-            personState = ('').join(foundCityGroup[1])
+def main():
+    fileName = raw_input('What is the name of the list of appproved invoices? ')
+    file = open(fileName,"r")
+    item = 0
+    for line in file:
+        #Skip white space
+        while space.match(line):
+            line = file.next()
+        line = pageCheck(file,line)
+        name = line
+        print(name)
+        line = pageCheck(file,line)
+        ssnLine = file.next()
+        ssnLine = pageCheck(file,ssnLine)
+        foundSSN = SSN.search(ssnLine)
+        foundAddress = address.search(ssnLine)
+        foundApt = apt.search(ssnLine)
+        if foundSSN:
+            personSSN = foundSSN.group(0)
+            print(personSSN)
+        else: 
+            personSSN = False
+            print("Invalid SSN")
+        if foundAddress:
+            personFoundAddress = foundAddress.group(0)
+            print("The address is " + foundAddress.group(0))
         else:
-            personState = "Regex error"
-        print("CITY: "+('').join(foundCityGroup[0]))
-        print("STATE: "+('').join(foundCityGroup[1]))
-        print("The line is " + line) 
-    foundZipcode = zipcode.findall(line)
-    if foundZipcode[1]:
-        personZipcode = foundZipcode[1][0]
-        print("ZIPCODE: "+personZipcode)
-    else:
-        print("Could not find regex obj")
-        personZipcode = "Regex error"
-    currentline = file.next()
-    #Finding the YTD Total section
-    while not(total.search(currentline)):
+            personFoundAddress = "ADDRESS REGEX DID NOT MATCH"
+        if foundApt:
+            personApt = foundApt.group(0)
+            print ("The apartment number is " + personApt)
+        else:
+            personApt = ""
+        cityLine=file.next()
+        line = cityLine
+        line = pageCheck(file,line)
+        foundCityGroup = city.findall(line)
+        if foundCityGroup:
+            personCity = ('').join(foundCityGroup[0])
+            if foundCityGroup[1]:
+                personState = ('').join(foundCityGroup[1])
+            else:
+                personState = "Regex error"
+            print("CITY: "+('').join(foundCityGroup[0]))
+            print("STATE: "+('').join(foundCityGroup[1]))
+            print("The line is " + line) 
+        foundZipcode = zipcode.findall(line)
+        if foundZipcode[1]:
+            personZipcode = foundZipcode[1][0]
+            print("ZIPCODE: "+personZipcode)
+        else:
+            print("Could not find regex obj")
+            personZipcode = "Regex error"
         currentline = file.next()
-    cashCheck = cash.findall(currentline)
-    #print(cashCheck)
-    if cashCheck:
-        cashTotal = float(cash.search(currentline).group(0).replace(',',''))
-        if cashCheck[0][3]:
-            change = cashCheck[0][3].replace('.','')
+        #Finding the YTD Total section
+        while not(total.search(currentline)):
+            currentline = file.next()
+        cashCheck = cash.findall(currentline)
+        #print(cashCheck)
+        if cashCheck:
+            cashTotal = float(cash.search(currentline).group(0).replace(',',''))
+            if cashCheck[0][3]:
+                change = cashCheck[0][3].replace('.','')
+            else:
+                change = "00"
         else:
-            change = "00"
-    else:
-        cashTotal = "Regex error"
-   
-    
-    
-    if personSSN and personSSN not in ssnList and cashTotal > 600:
-        firstName = nameCombo.search(name).group(3)
-        lastName = nameCombo.search(name).group(1)
-        adr = ADR.search(personFoundAddress).group(0)
-        streetName = streetNameRegex.search(personFoundAddress).group(0)
-        if firstName is None:
-            firstName = "DID NOT MATCH REGEX"
-        if lastName is None:
-            lastName = "DID NOT MATCH REGEX"
-        if adr is None:
-            adr = ""
+            cashTotal = "Regex error"
+       
         
-        item +=1
-        print("Add to worksheet")
-        output.write("Number: "+str(item) + "\n")
-        output.write(name+"\n")
-        output.write("FIRSTNAME: " + firstName + " LASTNAME: " + lastName + "\n")
-        output.write(personSSN + "\n")
-        output.write(personFoundAddress + "\n")
-        output.write("ADR: " + adr + "\n")
-        output.write("Street Name " + streetName + "\n")
-        output.write(personCity + "\n")
-        output.write(personState + "\n")
-        output.write(personZipcode + "\n")
-        output.write(str(cashTotal) + "\n") 
-        output.write("-"*20)
-        output.write("\n")
-        contentList = [firstName.upper(),' ',lastName.upper(),personSSN,adr,streetName,personApt,personCity,personState,personZipcode,str(int(cashTotal)),str(change) ,' ',]
-        counter = 0
-        for cellRow in outputSheet["A"+str(item):"M"+str(item)]:
-            for cell in cellRow:
-                cell.value = contentList[counter]
-                #print(counter)
-                counter += 1
-    print("-------------------------------")
+        
+        if personSSN and personSSN not in ssnList and cashTotal > 600:
+            firstName = nameCombo.search(name).group(3)
+            lastName = nameCombo.search(name).group(1)
+            adr = ADR.search(personFoundAddress).group(0)
+            streetName = streetNameRegex.search(personFoundAddress).group(0)
+            if firstName is None:
+                firstName = "DID NOT MATCH REGEX"
+            if lastName is None:
+                lastName = "DID NOT MATCH REGEX"
+            if adr is None:
+                adr = ""
+            
+            item +=1
+            print("Add to worksheet")
+            output.write("Number: "+str(item) + "\n")
+            output.write(name+"\n")
+            output.write("FIRSTNAME: " + firstName + " LASTNAME: " + lastName + "\n")
+            output.write(personSSN + "\n")
+            output.write(personFoundAddress + "\n")
+            output.write("ADR: " + adr + "\n")
+            output.write("Street Name " + streetName + "\n")
+            output.write(personCity + "\n")
+            output.write(personState + "\n")
+            output.write(personZipcode + "\n")
+            output.write(str(cashTotal) + "\n") 
+            output.write("-"*20)
+            output.write("\n")
+            contentList = [firstName.upper(),' ',lastName.upper(),personSSN,adr,streetName,personApt,personCity,personState,personZipcode,str(int(cashTotal)),str(change) ,' ',]
+            counter = 0
+            for cellRow in outputSheet["A"+str(item):"M"+str(item)]:
+                for cell in cellRow:
+                    cell.value = contentList[counter]
+                    #print(counter)
+                    counter += 1
+        print("-------------------------------")
     
 
-
+main()
 wbSave.save("export.xlsx")
 output.close()
 
